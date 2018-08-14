19737ebc55e1219ffb63cc9faf613f7d532442e4
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-15 01:55:04 +0700

    Add validates presence of address in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :email, :password_confirmation,
+  validates_presence_of :name, :email, :password_confirmation, :address,
                         :birth_place, :birth_date
 end
