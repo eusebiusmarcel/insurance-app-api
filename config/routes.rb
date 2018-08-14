0a4437d04861a326acba105da112f404771bce8a
igihcksn <igihcksn@gmail.com>
2018-08-14 22:02:25 +0700

    [Create Api Login User & Admin]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  post 'admin_token' => 'admin_token#create'
-  post 'user_token' => 'user_token#create'
   namespace :v1 do
+    # == User ==
+    get 'users' => 'users#index'
+    get 'user' => 'users#show'
+    post 'user/login' => 'user_token#create'
+    post 'user/create' => 'users#create'
+    put 'user/:id/update' => 'users#update'
+    delete 'user/:id/destroy' => 'users#destroy'
+
+    # == Admin ==
+    get 'admins' => 'admins#index'
+    get 'admin' => 'admins#show'
+    post 'admin/login' => 'admin_token#create'
+    post 'admin/create' => 'admins#create'
+    put 'admin/:id/update' => 'admins#update'
+    delete 'admin/:id/destroy' => 'admins#destroy'
   end
 end
