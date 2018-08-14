1a75fa1bebf1ba3efc954a6be26e4ec15a3941ee
igihcksn <igihcksn@gmail.com>
2018-08-14 22:02:25 +0700

    [Create Api Login User & Admin]

--- app/controllers/user_token_controller.rb
+++
@@ -1,3 +0,0 @@
-class UserTokenController < Knock::AuthTokenController
-    
-end
---
+++ app/controllers/v1/user_token_controller.rb
@@ -0,0 +1,3 @@
+class V1::UserTokenController < Knock::AuthTokenController
+    skip_before_action :verify_authenticity_token
+end
