36cbf07c13b879c44d6f36d7a2bc07b70f9d70c1
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-14 15:12:00 +0700

    Add user and admin model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates_presence_of :name, :email, :password_digest
+end
---
+++ db/migrate/20180814080634_create_admins.rb
@@ -0,0 +1,14 @@
+class CreateAdmins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :admins do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :address
+      t.string :place_of_birth
+      t.string :date_of_birth
+
+      t.timestamps
+    end
+  end
+end
