5cd6a10438e9e11bfdaeb13a8bace4f117808910
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-15 01:51:31 +0700

    Fix conflict

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,9 +1,5 @@
 class Admin < ApplicationRecord
   has_secure_password
-<<<<<<< HEAD
   validates_presence_of :name, :email, :password_confirmation, :address,
                         :birth_place, :birth_date
-=======
-  validates_presence_of :name, :email, :password_confirmation
->>>>>>> f9bafd95049b82aa7f79ce87698bd1aeee7fb895
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-<<<<<<< HEAD
   validates_presence_of :name, :email, :password_confirmation,
                         :birth_place, :birth_date
-=======
-  validates_presence_of :name, :email, :password_confirmation
->>>>>>> f9bafd95049b82aa7f79ce87698bd1aeee7fb895
 end
