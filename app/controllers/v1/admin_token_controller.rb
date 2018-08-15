e4604a2ee5ae4af9310d2c57f0936335f4469e6c
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-15 23:27:47 +0700

    Add status message: "Login succeed" for user and admin login

--- app/controllers/v1/admin_token_controller.rb
+++ app/controllers/v1/admin_token_controller.rb
@@ -1,3 +1,7 @@
 class V1::AdminTokenController < Knock::AuthTokenController
     skip_before_action :verify_authenticity_token
+    
+    def create
+        render json: { status: "Login succeed", jwt: auth_token.token }, status: :created
+    end
 end
