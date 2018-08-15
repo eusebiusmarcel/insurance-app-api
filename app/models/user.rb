d5b497b9da7cbdcd1522f5211a7bd4bb225bc6e1
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-15 12:11:34 +0700

    Resolve conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :email, :password_confirmation
+  validates_presence_of :name, :email, :password_confirmation,
+                        :birth_place, :birth_date
 end
