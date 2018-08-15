998dabcd93b2e6ba2533e277d3c00ca76d369e7e
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-15 23:36:36 +0700

    Add validations for users' and admins' name, email, and password

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,7 @@
 class Admin < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :email, :password_confirmation, :address,
-                        :place_of_birth, :date_of_birth
+  validates :name, presence: true, length: { in: 3..50 }, format: { with: NAME_REGEX, message: 'must include letter' } 
+  validates :email, presence: true, format: { with: EMAIL_REGEX }, uniqueness: true
+  validates :password, allow_nil: true, length: { in: 6..30 }
+  validates_presence_of :password_confirmation, :address, :place_of_birth, :date_of_birth
 end
