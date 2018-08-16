09796eab3ed62555419a92d43a9661c2446efddf
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-17 04:02:04 +0700

    Add id card number and gender to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  include PasswordManagement
   before_save { email.downcase! }
   has_secure_password
   validates :name, presence: true, length: { in: 3..50 }, format: { with: NAME_REGEX, message: 'must include letter' } 
   validates :email, presence: true, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { in: 6..30 }, allow_nil: true
-  validates_presence_of :address, :place_of_birth, :date_of_birth
+  validates_presence_of :address, :place_of_birth, :date_of_birth, :id_card_number
+  validates :gender, inclusion: { in: %w[perempuan laki_laki], message: 'perempuan atau laki_laki?' }
+  enum gender: { perempuan: 0, laki_laki: 1 }
 end
