877d767031c0abf87d3a26258831a82055e44c3d
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-19 22:37:08 +0700

    Fix PHONE_REGEX, ID_NUMBER_REGEX, and rescue ArgumentError for enums.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
+  def initialize(*)
+    super
+  rescue ArgumentError
+    raise if valid?
+  end
+
   self.abstract_class = true
 
   EMAIL_REGEX = /\A[\w+\-.]+@(?!mailinator.com)[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  PHONE_REGEX = /(\()?(\+62|62|0)(\d{2,3})?\)?[ .-]?\d{2,4}[ .-]?\d{2,4}[ .-]?\d{2,4}/x
+  PHONE_REGEX = /\A(\()?(\+62|62|0)(\d{2,3})?\)?[ .-]?\d{2,3}[ .-]?\d{2,3}[ .-]?\d{2,3}\z/x
+  ID_NUMBER_REGEX = /\A\d{16}+\z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   has_secure_password
   has_many :polishes
   validates :name, presence: true, length: { in: 3..50 }
-  validates :email, presence: true, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: EMAIL_REGEX }, 
+                    uniqueness: {case_sensitive: false }
   validates :password, presence: true, length: { in: 6..30 }, allow_nil: true
-  validates :id_card_number, presence: true, uniqueness: true
-  validates :gender, inclusion: { in: %w[perempuan laki_laki], message: 'perempuan atau laki_laki?' }
+  validates :id_card_number, presence: true, uniqueness: true, format: {
+    with: ID_NUMBER_REGEX, message: 'terdiri dari 16 angka yang tertera di KTP' }
+  validates :gender, inclusion: { in: %w[perempuan laki_laki],
+                                  message: 'perempuan atau laki_laki?' }
   enum gender: { perempuan: 0, laki_laki: 1 }
   validates :phone_number, presence: true, format: { with: PHONE_REGEX }
   validates_presence_of :address, :place_of_birth, :date_of_birth
