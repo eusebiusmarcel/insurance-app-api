c777a29a018ff3ca280a3ca62bb45c0bf6758c9b
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-20 18:46:48 +0700

    Change password when email is updated, and send the new password to new email.

--- app/controllers/v1/admins_controller.rb
+++ app/controllers/v1/admins_controller.rb
@@ -45,7 +45,13 @@ class V1::AdminsController < ApplicationController
 
     def update_user
         user = User.find(params[:id])
+        old_email = user.email
         user.update!(update_user_params)
+        unless params[:email].blank? || user.email == old_email
+            user.password = user.generate_token
+            user.save!
+            UserMailer.with(user: user).change_email.deliver
+        end
         render json: { status: "User berhasil diupdate", result: user.as_json(except:
             %i[password_digest reset_password_token reset_password_token_sent_at]) },
                status: :ok
