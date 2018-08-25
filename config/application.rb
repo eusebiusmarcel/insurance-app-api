0d694e85a52ea7c2b0e7bb9f2bb673b2a76987db
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-25 20:45:01 +0700

    Fix problem from FE: CORS header ‘Access-Control-Allow-Origin’ missing

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,10 @@ module QuindApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'http://quind-api.herokuapp.com',
+      'Access-Control-Request-Method' => %w{GET POST PUT DELETE}.join(",")
+    }
   end
 end
