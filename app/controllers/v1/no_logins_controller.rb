ef2db416193e072d66189531a36142583a0fa05e
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-25 21:28:17 +0700

    Update json for no login collection per FE request.

--- app/controllers/v1/no_logins_controller.rb
+++ app/controllers/v1/no_logins_controller.rb
@@ -1,27 +1,27 @@
 class V1::NoLoginsController < ApplicationController
   def index_user
     users = User.all.order(:id)
-    render json: { status: 'OK', users: users }, status: :ok
+    render json: { users: users }, status: :ok
   end
 
   def index_policy
     policies = Policy.all.order(:id)
-    render json: { status: 'OK', policies: policies }, status: :ok
+    render json: { policies: policies }, status: :ok
   end
 
   def show_policies_of_one_user
     user = User.find(params[:id])
     policies = user.policies.all.order(:id)
-    render json: { status: 'OK', policies: policies }, status: :ok
+    render json: { policies: policies }, status: :ok
   end
 
   def show_user
     user = User.find(params[:id])
-    render json: { status: 'OK', user: user }, status: :ok
+    render json: { user: user }, status: :ok
   end
 
   def show_policy
     policy = Policy.find(params[:id])
-    render json: { status: 'OK', policy: policy }, status: :ok
+    render json: { policy: policy }, status: :ok
   end
 end
