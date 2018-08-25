9701b1d5d9c39dcbc9795d7d98519d6202bcbf35
Eusebius Marcel <eusebius.em@gmail.com>
2018-08-25 21:03:42 +0700

    Fix problem from FE: CORS header ‘Access-Control-Allow-Origin’ missing (Try 3)

--- config/application.rb
+++ config/application.rb
@@ -31,16 +31,5 @@ module QuindApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    module QuindApi
-      class Application < Rails::Application
-        config.middleware.insert_before 0, Rack::Cors do
-          allow do
-            origins '*'
-            resource '*', headers: :any, methods: %i[get post put delete]
-          end
-        end
-      end
-    end
   end
 end
