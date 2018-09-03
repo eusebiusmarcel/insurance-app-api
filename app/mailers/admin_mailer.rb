7468f13ea7ca2bd11a0c65111dd400d26407d11f
Eusebius Marcel <eusebius.em@gmail.com>
2018-09-03 23:01:22 +0700

    Change link for user and admin forgot password.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,7 +1,7 @@
 class AdminMailer < ApplicationMailer
   before_action :set_attributes
   def forgot_password
-    @url = "https://quind-api.herokuapp.com/v1/admin/reset/password/#{@admin.reset_password_token}"
+    @url = "quind://admin/reset/password/#{@admin.reset_password_token}"
     mail to: @email_with_name, subject: "Hi #{@admin.name}, this is your reset password link"
   end
 
