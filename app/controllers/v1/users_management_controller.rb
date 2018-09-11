bbf29bc09154ef955b4b5fe0103f09a2ebdd8046
Eusebius Marcel <eusebius.em@gmail.com>
2018-09-11 16:41:10 +0700

    Remove unneeded private method

--- app/controllers/v1/users_management_controller.rb
+++ app/controllers/v1/users_management_controller.rb
@@ -28,11 +28,6 @@ class V1::UsersManagementController < ApplicationController
 
   private
 
-  def user_params
-    params.permit(:name, :email, :id_card_number, :gender, :city,
-                  :address, :phone_number, :place_of_birth, :date_of_birth)
-  end
-
   def update_user_params
     params.permit(:email, :address, :phone_number)
   end
