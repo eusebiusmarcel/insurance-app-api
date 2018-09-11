1b2d6cf2b15d69b71165b8bc7903a136cc876f9b
Eusebius Marcel <eusebius.em@gmail.com>
2018-09-11 15:38:58 +0700

    Add feature claim

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    # User Collections
+    # User Collection
     get 'user' => 'users#show'
     post 'user/login' => 'authentications#authenticate_user'
     put 'user/update/password' => 'users#update_password'
@@ -9,32 +9,38 @@ Rails.application.routes.draw do
     put 'user/avatar/upload' => 'users#upload_avatar'
     delete 'user/avatar/delete' => 'users#delete_avatar'
 
-    # Admin Collections
+    # User Policy Collection
+    get 'user/policies' => 'user_policies#index'
+    get 'user/policies/:id' => 'user_policies#show'
+
+    # User Claim Collection
+    get 'user/claims' => 'user_claims#index'
+    get 'user/claims/:id' => 'user_claims#show'
+
+    # Admin Collection
     get 'admin' => 'admins#show'
     post 'admin/login' => 'authentications#authenticate_admin'
     put 'admin/update/password' => 'admins#update_password'
     post 'admin/forgot/password' => 'admins#forgot_password'
     post 'admin/reset/password/:token' => 'admins#reset_password'
 
-    # User Management Collections
+    # User Management Collection
     post 'admin/user/import/csv' => 'users_management#create_by_csv'
     get 'admin/users' => 'users_management#index'
     put 'admin/user/update/:id' => 'users_management#update'
 
-    # Policy Collections
+    # Policy Management Collection
     get 'admin/policies' => 'policies_management#index'
     get 'admin/policies/user/:id' => 'policies_management#show_policies_of_one_user'
     put 'admin/policies/:id/document/upload' => 'policies_management#upload_policy_document'
     post 'admin/policies/import/csv' => 'policies_management#create_by_csv'
-    get 'user/policies' => 'user_policies#index'
-    get 'user/policies/:id' => 'user_policies#show'
 
-    # Payment Detail
+    # Payment Detail Collection
     get 'admin/payment/detail/:policy_id' => 'payment_detail#detail'
     get 'user/payment/detail/:policy_id' => 'payment_detail#detail_by_user'
     post 'admin/payment/create' => 'payment_detail#create'
     
-    # Guest Collections
+    # Guest Collection
     post 'guest/create' => 'guests#create'
     get 'guests' => 'guests#index'
     get 'guest/:id' => 'guests#show'
