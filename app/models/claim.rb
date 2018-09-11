c3fbeda463aaa54051cb34dd8d6e67722356b48a
Eusebius Marcel <eusebius.em@gmail.com>
2018-09-11 22:01:23 +0700

    Add insurance type and policy number into claim as_json

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -8,4 +8,16 @@ class Claim < ApplicationRecord
 
   validates :amount, presence: true
   validate :status_should_be_valid
+
+  def as_json(*)
+    super(methods: %i[policy_number insurance_type])
+  end
+
+  def insurance_type
+    policy.insurance_type
+  end
+
+  def policy_number
+    policy.policy_number
+  end
 end
