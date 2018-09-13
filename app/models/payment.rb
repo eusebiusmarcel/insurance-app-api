ac86c8c3b6a9ae81a138c4220b072b53a8453f8b
Eusebius Marcel <eusebius.em@gmail.com>
2018-09-13 21:47:28 +0700

    [Marcel] Resolve typo of policy_number

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,11 +3,11 @@ class Payment < ApplicationRecord
   belongs_to :user
 
   def as_json(*)
-    super(methods: %i[police_number premium_per_month])
+    super(methods: %i[policy_number premium_per_month])
   end
 
-  def police_number
-    policy.police_number
+  def policy_number
+    policy.policy_number
   end
 
   def premium_per_month
