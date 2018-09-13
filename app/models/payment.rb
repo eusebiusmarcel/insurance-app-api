af0145ebc9aafd2978b8c8a0f943749ef3ea23b9
Eusebius Marcel <eusebius.em@gmail.com>
2018-09-13 21:35:39 +0700

    [Marcel] Add premium_per_month in payments as_json

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,12 @@
 class Payment < ApplicationRecord
   belongs_to :policy
   belongs_to :user
+
+  def as_json(*)
+    super(methods: %i[premium_per_month])
+  end
+
+  def premium_per_month
+    policy.premium_per_month
+  end
 end
