6bd58a17497ba33eeecf51a9a4f2015e23bd54f9
Eusebius Marcel <eusebius.em@gmail.com>
2018-09-14 00:18:46 +0700

    [Marcel] Auto add document url for policies; remove policy document uploader

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -2,7 +2,6 @@ class Policy < ApplicationRecord
   include ValidateInsuranceType
   include ValidatePolicyStatus
 
-  mount_uploader :document_url, PolicyDocumentUploader
   before_save{ policy_number.upcase! }
 
   belongs_to :user
@@ -43,6 +42,7 @@ class Policy < ApplicationRecord
       policy.balance = policy.limit_per_year
       policy.payment_due_date = Time.now.strftime('%d').to_i if Time.now.strftime('%d').to_i <= 28
       policy.payment_due_date = 28 if Time.now.strftime('%d').to_i > 28
+      policy.document_url = 'https://res.cloudinary.com/quind-cloud/image/upload/v1536858697/SPECIMEN-Quind_Policy.pdf'
       if policy.save
         UserMailer.with(user: user, policy: policy).policy_registered.deliver
         created_policies.push(policy)
